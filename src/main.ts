9cb3ec0d874b2a5509e99c82845d17a90d33512e
Svyatoslav <s.denisyuk@ucs-service.ru>
2022-03-16 18:12:17 +0300

    commit 17 CORS fix

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,6 @@ import {AppModule} from "./app.module";
 async function start() {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule)
-    app.enableCors({
-      origin: true,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      credentials: true,
-    });
 
     await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
 }
